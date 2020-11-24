c1a39ddafe38ff2f5ea11b02f8eb50ff274af567
Sameer Khan <sameerk_786@hotmail.com>
2020-11-24 03:33:07 +0000

    coverted all string outputs to template literals format

--- assertEqual.js
+++ assertEqual.js
@@ -6,11 +6,11 @@ const assertEqual = function(actual, expected) {
   // if actual value matches expected value then print out assertion passed message
   if (actual === expected) {
 
-    console.log("😀️😀️😀️ Assertion Passed: " + actual + " === " + expected);
+    console.log(`😀️😀️😀️ Assertion Passed: ${actual} === ${expected}`);
 
   } else {
     // print out assertion failed message if actual value does not match expected value
-    console.log("😡️😡️😡️ Assertion Failed: \"" + actual + "\" !== \"" + expected + "\"");
+    console.log(`😡️😡️😡️ Assertion Failed: \"${actual}\" !== \"${expected}\"`);
 
   }
 };
