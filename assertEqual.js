48b9a67d43e3290558545d012bd7cfc0473e0be0
Sameer Khan <sameerk_786@hotmail.com>
2020-11-24 03:17:17 +0000

    Added code to assertEqual.js to print assert fail or pass messages

---
+++ assertEqual.js
@@ -0,0 +1,32 @@
+
+// Function implementation
+
+const assertEqual = function(actual, expected) {
+
+  // if actual value matches expected value then print out assertion passed message
+  if (actual === expected) {
+
+    console.log("😀️😀️😀️ Assertion Passed: " + actual + " === " + expected);
+
+  } else {
+    // print out assertion failed message if actual value does not match expected value
+    console.log("😡️😡️😡️ Assertion Failed: \"" + actual + "\" !== \"" + expected + "\"");
+
+  }
+};
+
+
+// TEST CODE
+
+// comparing non-identical strings
+assertEqual("Lighthouse Labs", "Bootcamp");
+
+// comparing identical numbers
+assertEqual(1,1);
+
+// comparing non-identical numbers
+assertEqual(2,3);
+
+// comparing identical strings
+assertEqual("cool","cool");
+
