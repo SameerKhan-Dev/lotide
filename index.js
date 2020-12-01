0d5129475c8c03302060456cc804f5c393c4701a
Sameer Khan <sameerk_786@hotmail.com>
2020-12-01 14:36:30 +0000

    simplified index.js through use of property value shorthand syntax from ES6

--- index.js
+++ index.js
@@ -24,6 +24,52 @@ const assertArraysEqual = require('./assertArraysEqual');
 
 
 module.exports = {
+  head,
+  tail,
+  middle,
+  without,
+  takeUntil,
+  map,
+  letterPositions,
+  flatten,
+  findKeyByValue,
+  findKey,
+  eqObjects,
+  eqArrays,
+  countOnly,
+  countLetters,
+  assertObjectsEqual,
+  assertEqual,
+  assertArraysEqual
+};
+
+
+/*
+  Using normal notation: above shorthand notation for object is equivalent to:
+  // index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const flatten = require('./flatten');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+
+
+
+
+
+module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
@@ -42,3 +88,17 @@ module.exports = {
   assertEqual: assertEqual,
   assertArraysEqual: assertArraysEqual
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
