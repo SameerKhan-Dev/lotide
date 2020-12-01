ed42d2b56e0d7613989c5ca8ad0c6608f115a1fe
Sameer Khan <sameerk_786@hotmail.com>
2020-12-01 13:51:10 +0000

    incorporated test cases with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,36 +1,83 @@
 
+const assert = require('chai').assert;
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
-//const eqArrays = require('../eqArrays');
 
 
-// TEST CASES //
+
+
 
 ///////TEST CASES -- All should have Assertion passed if our middle function is functioning correctly.////////
 
-// odd (7) elements
-assertArraysEqual(middle([0,1,2,3,4,5,6]), [3]); // expected Assertion Passed for all cases
-// odd (7) elements
-assertArraysEqual(middle([19,1,5,22,1,3,5]), [22]); // expected Assertion Passed for all cases
-// odd (9) elements
-assertArraysEqual(middle([23,1,5,73,1,5,5]), [73]); // expected Assertion Passed for all cases
-// even (4) elements
-assertArraysEqual(middle([28,1,3,45]), [1,3]); // expected Assertion Passed for all cases
-// even (10) elements
-assertArraysEqual(middle([18,12,4,5,6,7,7,9,10,23]), [6,7]); // expected Assertion Passed for all cases
-// empty array
-assertArraysEqual(middle([]), []); // expected Assertion Passed for all cases
-// array with 1 element only
-assertArraysEqual(middle([19]), []); // expected Assertion Passed for all cases
-// array with 2 elements only
-assertArraysEqual(middle([1,5]), []); // expected Assertion Passed for all cases
 
-// some other test cases:
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
+describe("#head", () => {
 
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
+  it("testing odd elements size array, should return single correct middle value ", () => {
+    // odd (7) elementse
+    assert.deepEqual(middle([0,1,2,3,4,5,6]), [3]); // expected Assertion Passed for all cases
+    // odd (7) elements
+    assert.deepEqual(middle([19,1,5,22,1,3,5]), [22]); // expected Assertion Passed for all cases
+    // odd (9) elements
+    assert.deepEqual(middle([23,1,5,73,1,5,5]), [73]); // expected Assertion Passed for all cases
 
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+    // odd (3) elements 
+    assert.deepEqual(middle([1,2,3]), [2]);
+    // odd (5) elements
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+
+  
+  });
+
+  it("testing even elements size array, should return two middle values", () => {
+
+  // even (4) elements
+    assert.deepEqual(middle([28,1,3,45]), [1,3]); // expected Assertion Passed for all cases
+  // even (10) elements
+    assert.deepEqual(middle([18,12,4,5,6,7,7,9,10,23]), [6,7]); // expected Assertion Passed for all cases   
+  
+  // even (4) elements
+  assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  
+  // even (6) elements
+  assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);
+
+  });
+
+
+
+  it("Should return an empty array, when input is an empty array, array with single or two elements only", () => {   
+    // empty array
+    assert.deepEqual(middle([]), []); // expected Assertion Passed for all cases
+    // array with 1 element only
+    assert.deepEqual(middle([1]), []); // expected Assertion Passed for all cases
+    // array with 2 elements only
+    assert.deepEqual(middle([1,2]), []); // expected Assertion Passed for all cases
+   
+ 
+ 
+  });
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,29 +1,111 @@
+
+const assert = require('chai').assert;
+
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+
+
+describe("#head", () => {
+
+  it("Make sure when input is [hello, lighthouse, Labs] the resulting length is 2, and first element is lighthouse and second element is labs", () => {
+
+    // assert.strictEqual(head([5,6,7]),5);
+
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+    assert.strictEqual(result.length, 2); // if true makes sure we get back two elements.
+    console.log("Hello");
+    assert.strictEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+    console.log("Bye");
+    assert.strictEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+  });
+
+  it("Make sure when input is [Yoyo, lighthouse, Labs] the original array has not changed", () => {
+
+    // assert.strictEqual(head([5,6,7]),5);
+    const words = ["Yoyo", "Lighthouse", "Labs"];
+
+    const result = tail(words);
+
+    assert.strictEqual(words.length, 3); // if passed, means original array has not been changed
+
+  });
+
+
+  // Testing Empty Array
+
+  it("Make sure empty array input returns an empty array output", () => {   
+
+    const originalArray = [];
+
+    const result = tail(originalArray);
+
+    assert.deepEqual(originalArray,result);
+  
+  });
+  it("Make sure empty array input returns an empty array output", () => {   
+
+    const originalArray = [];
+
+    const result = tail(originalArray);
+
+    assert.deepEqual(originalArray,result);
+  
+  });
+
+// Testing Array with single element
+const returnedArray2 = tail([]);
+console.log(returnedArray2);
+  it("Make sure an array with only a single element returns an empty array output", () => {   
+
+    const originalArray = [1];
+
+    const result = tail(originalArray);
+
+    assert.deepEqual(result,[]);
+
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 
 
 // running Test Cases
 
 // Test Case 1: Check the Returned Array Elements
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-assertEqual(result.length, 2); // if true makes sure we get back two elements.
-
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
 
 
 // Test Case 2: Check the Original Array
 
-const words = ["Yoyo", "Lighthouse", "Labs"];
-
-
-tail(words);
-
-assertEqual(words.length, 3); // if passed, means original array has not been changed
 
 
 
@@ -32,26 +114,12 @@ assertEqual(words.length, 3); // if passed, means original array has not been ch
 
 // Test Case 3: Write a test Case to make sure that the original array is not modified
 
-const originalArray = ["Hello", "Lighthouse", "Labs"];
 
-
-tail(originalArray);
-
-assertEqual(originalArray[0], "Hello"); //check first element of original array to make sure its not changed
-
-assertEqual(originalArray[1], "Lighthouse");  // check second element of original array to make sure its not changed
-
-assertEqual(originalArray[2], "Labs"); // check third element of original array to make sure its not changed
 
 
 /*
-// Testing Empty Array
 
-const returnedArray = tail([]);
-console.log(returnedArray);
 
-// Testing Array with single element
-const returnedArray2 = tail([]);
-console.log(returnedArray2);
 
 */
+
